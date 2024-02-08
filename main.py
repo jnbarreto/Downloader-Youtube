a70f796496135633f002bb9e6665a4720c0fde3c
Juan Carlos Leone Barreto <jc.leone98@gmail.com>
2024-02-08 18:06:28 -0300

    feat: check if alredy is installed dependencies

--- main.py
+++ main.py
@@ -1,29 +1,61 @@
-import PySimpleGUI as sg
 from pytube import YouTube
+import PySimpleGUI as sg
+import subprocess
+import sys
+import os
 
-#Create interface TELA
-sg.theme("Dark Blue 16")
+def check_dependencies():
+    try:
+        import PySimpleGUI
+        import pytube
+        print("Dependências já instaladas.")
+    except ImportError:
+        print("Dependências não encontradas. Instalando...")
+        install_dependencies()
 
-interface = [
-    [sg.Titlebar("Youtube DownLoad", None, "white", "gray")],
-    [sg.Text("URL")],
-    [sg.Input(size=(50,1), key="url")],
-    [sg.Button("Download")]
-]
 
-window = sg.Window("window", interface)
+def install_dependencies():
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao instalar dependências: {e}")
+        sys.exit(1)
 
-while True:
-    event, value = window.read()
 
-    if value == sg.WIN_CLOSED:
-        break
+def dowloader():
+    #Create interface TELA
+    sg.theme("Dark Blue 16")
 
-    if event == 'Download':
-        link = window["url"].get()
-        video = YouTube(link)
-        stream = video.streams.get_highest_resolution().download()
-    print("Download Finished")
+    interface = [
+        [sg.Titlebar("Youtube DownLoad", None, "white", "gray")],
+        [sg.Text("URL")],
+        [sg.Input(size=(50,1), key="url")],
+        [sg.Button("Download")]
+    ]
 
-window.close()
-exit()
+    window = sg.Window("window", interface)
+
+    while True:
+        event, value = window.read()
+
+        if value == sg.WIN_CLOSED:
+            break
+
+        if event == 'Download':
+            link = window["url"].get()
+            video = YouTube(link)
+            stream = video.streams.get_highest_resolution().download()
+        print("Download Finished")
+
+    window.close()
+    exit()
+
+
+if __name__ == "__main__":
+    # Verifica se o arquivo 'requirements.txt' está presente no mesmo diretório
+    if not os.path.isfile("requirements.txt"):
+        print("O arquivo 'requirements.txt' não foi encontrado.")
+        sys.exit(1)
+
+    check_dependencies()
+    dowloader()
